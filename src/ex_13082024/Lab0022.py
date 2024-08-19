dbde7e24dda949cb8b1443450c7c12a31cdf869d
Susmitha Pamarthi <sushmitha.p85@gmail.com>
2024-08-19 16:50:46 -0400

    2d Source added

---
+++ src/ex_13082024/Lab0022.py
@@ -0,0 +1,9 @@
+#String to Integer
+
+num1 = int(input("Enter num1"))
+num2 = int(input("Enter num2"))
+
+print("Sum is", num1+num2)
+print("Sub is", num1-num2)
+print("Mul is", num1*num2)
+print("Div is", num1/num2)
